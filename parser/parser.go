dad25c7c65d9922807a1b08bc28416ade9556b5f
Andrew Cutler <AndrewKCutler@gmail.com>
2024-04-13 13:16:21 -0400

    adde flags for src/dest

--- parser/parser.go
+++ parser/parser.go
@@ -2,35 +2,49 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"os"
 	"strings"
+
+	"github.com/google/uuid"
 )
 
-func splitIntoGames(filename string) {
+func splitGames(filename string, dest string) {
 	file, err := os.Open(filename)
 	if err != nil {
-		log.Println(err)
-		return
+		log.Panicln(err)
 	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 
 	gameNumber := 1
-	lineNum := 1
 	for scanner.Scan() {
 		line := scanner.Text()
 		// grab pgns for all games that don't include eval, for easy of parsing
-		if strings.HasPrefix(line, "1. ") && !strings.Contains(line, "%%eval") {
-			fmt.Printf("Game #%d:\n%s\n", gameNumber, line)
+		if strings.HasPrefix(line, "1. ") && !strings.Contains(line, "eval") {
+			pgnId := uuid.New().String()
+			os.WriteFile(dest+"/"+pgnId+".pgn", []byte(line), 0064)
 			gameNumber++
 		}
-		lineNum++
 	}
 }
 
 func main() {
-	splitIntoGames("./test.pgn")
+	var srcFile = flag.String("src", "", "Source PGN file to parse")
+	var dest = flag.String("dest", "", "Directory to output parsed games.")
+	flag.Parse()
+
+	if *srcFile == "" {
+		log.Panicln("Source pgn file is required.")
+	}
+	if *dest == "" {
+		wd, _ := os.Getwd()
+		*dest = wd + "\\output"
+		fmt.Println(*dest)
+	}
+
+	splitGames(*srcFile, *dest)
 }

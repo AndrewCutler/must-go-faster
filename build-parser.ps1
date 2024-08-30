b360357474788106f11ca3c859a5462b92dcd52c
Andrew Cutler <AndrewKCutler@gmail.com>
2024-08-29 22:42:48 -0400

    created css folder and removed unused crap

--- build-parser.ps1
+++
@@ -1,7 +0,0 @@
-# todo: output to bin
-$userProfile = $env:USERPROFILE
-if (-not (Test-Path -Path $userProfile\go\bin -PathType Container)) {
-    Write-Host "go bin does not exist somehow, creating..."
-    New-Item -ItemType Directory -Path $userProfile\go\bin -Force
-}
-go build -o $userProfile\go\bin\pgn-parser.exe .\backend\parser\parser.go
